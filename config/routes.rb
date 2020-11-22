de9d783a8d211674cb08463c823cae18a9721335
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-22 15:25:24 +0900

    Vレビュー　実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
   root 'tops#index'
    resources :items ,only: :index
     resources :actives ,only: :index
+     resources :reviews ,only: :index
 end
